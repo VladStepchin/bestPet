2f4d6df24c3bef3aacf9cff791c51f941d958114
Vlad Stepchyn <v.stepchyn@viseven.com>
2022-11-15 22:39:23 +0200

    add correct folder

---
+++ UI/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>Happy counting!</h1>
+    <p style="font-size: 40px">{{ count }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => {
+    return { count: 0 };
+  },
+  mounted: async function () {
+    await this.loadCount();
+  },
+  methods: {
+    loadCount: async function () {
+      let result = await axios.get("http://127.0.0.1:3000/");
+      this.count = result.data[0]._id;
+    },
+  },
+};
+</script>
